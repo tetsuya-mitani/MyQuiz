ebd28bb8fe800abd94c8e6a2877ad63983d02cf8
三谷 哲也 <mitani0328@hotmail.co.jp>
2016-12-31 16:18:15 +0900

    Initial Commit

---
+++ MyQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by 三谷 哲也 on 2016/12/31.
+//  Copyright © 2016年 Tetsuya Mitani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
